48cfee68498bb01960975e9f92bcac28d0658060
xianpeng yan <a973044869@163.com>
2022-04-17 15:47:37 +0800

    Changes

--- 20220415/src/TestDemo.java
+++ 20220415/src/TestDemo.java
@@ -1,11 +1,4 @@
 public class TestDemo {
-    public static void main(String[] args) {
-        for(int i=0;i<args.length;i++) {
-            System.out.println(args[i]);
-        }
-    }
-
-
     public static void fun1(String[] args) {
         System.out.println("Hello World");
         System.out.print("Hello World");
