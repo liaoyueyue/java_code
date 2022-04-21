614b45021fcaf73c0387e18317356601b864326f
xianpeng yan <a973044869@163.com>
2022-04-21 22:11:01 +0800

    Changes

---
+++ 20220421/src/TestDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: liaoyueyue
+ * Date: 2022-04-21
+ * Time: 22:10
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("这是一个推送的测试");
+    }
+}
