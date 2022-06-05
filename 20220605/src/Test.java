324c41346073db507ff536653d69574f270732bd
xianpeng yan <a973044869@163.com>
2022-06-05 11:14:41 +0800

    20220605

---
+++ 20220605/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: liaoyueyue
+ * Date: 2022-06-05
+ * Time: 10:59
+ */
+public class Test {
+        public static void main(String[] args) {
+            System.out.println("The size of short is "+Short.BYTES+" bytes.");
+            System.out.println("The size of int is "+Integer.BYTES+" bytes.");
+            System.out.println("The size of long is "+Long.BYTES+" bytes.");
+        }
+}
