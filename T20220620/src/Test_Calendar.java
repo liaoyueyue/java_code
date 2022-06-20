a0ca7696ff6b6842ed5f582a6d8f7523f46a261e
xianpeng yan <a973044869@163.com>
2022-06-20 17:18:11 +0800

    练习Calendar

--- T20220620/src/Test_Calendar.java
+++ T20220620/src/Test_Calendar.java
@@ -12,12 +12,15 @@ public class Test_Calendar {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int year = console.nextInt();
-        Calendar c = Calendar.getInstance();
-        for (int month = 1; month <= 12; month++) {
-            c.set(year, month, 1);
-            c.add(Calendar.DATE, -1);
-            int days = c.get(Calendar.DATE);
-            System.out.println(year + "年" + month + "月:" + days + "天");
+
+        //获取Calendar的实例
+        Calendar calendar=Calendar.getInstance();
+        //循环遍历所有的月份
+        for(int month=1;month<=12;month++){
+            //设置年、月、日
+            calendar.set(year,month,0);
+            //输出对应年份各个月的天数
+            System.out.println(year+"年"+month+"月:"+calendar.getActualMaximum(Calendar.DATE)+"天");
         }
     }
 }
